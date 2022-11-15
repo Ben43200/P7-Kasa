3079f513aca49952983d0c6608d5fca2cf89ec8c
Ben <meteoben@gmail.com>
2022-11-15 12:13:55 +0100

    stockdata sous forme de texte avec titre et description ok
    avec le maping data key

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -12,25 +12,43 @@ export default function Card (){
     const arr = Object.entries(stockData);
     console.log(arr);
 
-    return (
-        <ul>
-            {Object.keys(stockData).map((id) => {
-                const dayTasks = stockData[id];
-                return Object.keys(dayTasks).map((key) => {
-                    const task = dayTasks[key];
-                    return (
-                        <li>{task.name}</li>
-                    );
-                });
-            })}
-        </ul>
-    );
-
     // return (
-    //     <div>
-    //         <div>{Card}</div>
-    //         <div>{id}</div>
-
-    //     </div>
+    //     <ul>
+    //         {Object.keys(stockData).map((id) => {
+    //             const dayTasks = stockData[id];
+    //             return Object.keys(dayTasks).map((key) => {
+    //                 const task = dayTasks[key];
+    //                 return (
+    //                     <li>{task.name}</li>
+    //                 );
+    //             });
+    //         })}
+    //     </ul>
     // );
-}
+    return (
+        <>
+            <div className="stock-container">
+                {stockData.map((data, key) => {
+                    return (
+                        <div key={key}>
+                            {data.tags +
+                    " , " +
+                    data.location +
+                    " ," +
+                    data.description +
+                    ", " +
+                    data.title}
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+};
+// return (
+//     <div>
+//         <div>{Card}</div>
+//         <div>{id}</div>
+
+//     </div>
+// );
