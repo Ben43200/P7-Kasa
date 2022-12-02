fe5272679e889a217eb63da8cae19cdf58a75276
Ben <meteoben@gmail.com>
2022-12-02 19:44:47 +0100

    retourne un tableau en console avec les props, le système fonctionne mais n'affiche rien

--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -1,58 +1,7 @@
 import React, { useState } from "react";
-// import "./styles.css";
+import GetCard from "./SecondCard";
 
-// export default function App() {
-//     const [index, setIndex] = useState(0);
-
-//     const carousels = [
-//         "https://www.w3schools.com/css/img_5terre.jpg",
-//         "https://www.w3schools.com/css/img_forest.jpg",
-//         "https://www.w3schools.com/css/img_lights.jpg"
-//     ];
-
-//     const onNext = () => {
-//         console.log(carousels.length);
-
-//         if (index < carousels.length - 1) {
-//             const i = index + 1;
-//             setIndex(i);
-//         } else {
-//             setIndex(0);
-//         }
-//     };
-
-//     const onPrev = () => {
-//         if (index > 0 && index < carousels.length) {
-//             const i = index - 1;
-//             setIndex(i);
-//         } else {
-//             setIndex(carousels.length - 1);
-//         }
-//     };
-
-//     return (
-//         <div className="carousel">
-//             {carousels.map((item, i) =>
-//                 i === index
-//                     ? (
-//                         <div className="carousel-item" key={i}>
-//                             <img src={item} alt={item} />
-//                         </div>
-//                     )
-//                     : null
-//             )}
-
-//             <a className="prev" onClick={onPrev}>
-//         Prev
-//             </a>
-//             <a className="next" onClick={onNext}>
-//         Next
-//             </a>
-//         </div>
-//     );
-// };
-
-export default function Caroussel(props){
+export default function Carousel(props){
     console.log(props);
     const [position, setPosition] = useState(0);
     const carousels = props.images;
@@ -69,7 +18,7 @@ export default function Caroussel(props){
 
     return (
         <>
-            <img src={carousels[position]} alt="super image" />
+            <img src={`${carousels[position]}`} alt="super image" />
             <span onClick={decrement}>  > </span>
             <span>{position +1} / {carousels.length}</span>
             <span onClick={increment}> $ </span>
--- src/pages/SecondCard.js
+++ src/pages/SecondCard.js
@@ -1,42 +1,31 @@
 import React from "react";
 import { stockData } from "../data";
 import Cards from "./SecondCard";
-import Caroussel from "./Carousel";
+import Carousel from "./Carousel";
 import { Link, useParams } from "react-router-dom";
 
 const jsonString = JSON.stringify(stockData);
 // console.log(jsonString);
 
-export default function GetCard (){
-    const params = useParams();
+export default function GetCard (props){
+    // const params = useParams();
 
-    console.log(params); // 👉️ {userId: '4200'}
-    const listImages = [
-        "https://www.w3schools.com/css/img_5terre.jpg",
-        "https://www.w3schools.com/css/img_forest.jpg",
-        "https://www.w3schools.com/css/img_lights.jpg"
-    ];
+    console.log(props); // 👉️ {userId: '4200'}
+    const images = stockData.map(item => {
+
+        return (
+            <GetCard
+                img ={`${item.cover}`}
+
+                key={"card"+item.id}
+
+            />
+        );
+    });
 
     return (
-        <>
-            <div className="stock-container">
-                {stockData.map((data, key) => {
-                    return (
-                        <div key={key}>
-                            {data.tags +
-                    " , " +
-                    data.location +
-                    " ," +
-                    data.description +
-                    ", " +
-                    data.title}
-                        </div>
-                    );
-                })}
-                <h2>userId is 👉️ {params.userId}</h2>;
-                <Caroussel images={listImages} />
 
-            </div>
-        </>
+        <Carousel images= {images} />
+
     );
 };
