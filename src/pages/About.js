7a00046b47f004c4148301926e5a6089325f6f2d
Ben <meteoben@gmail.com>
2023-01-08 18:35:50 +0100

    last commit mise en place de la page about avec création fichier json pour les about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import Collapsible from "./Collapsible";
+import aboutTab from "../Tableau.json";
 
-export default function About(){
-    return (<div>
-        <h1>A Propos</h1>
-        <Link to="/">Accueil</Link>
-    </div>
+export default function About() {
+    return (
+
+        <div>
+            <h1>A Propos</h1>
+            <Link to="/">Accueil</Link>
+            {aboutTab.map((rule, id) => (
+                <Collapsible
+                    key={id}
+                    aboutTitle={rule.Title}
+                    aboutText={rule.Text}
+                    aboutStyle="about-style"
+                />
+
+            ))}
+        </div>
     );
 }
