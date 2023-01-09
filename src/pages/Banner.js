4c6a200b38eebe6fed8094f997ac6fdb03545344
Ben <meteoben@gmail.com>
2023-01-09 06:16:38 +0100

    ajout logo et background-banner

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from "../images/assets_images/LOGO.svg";
+import background from "../images/assets_images/background-banner.png";
 
 export default function Banner() {
     return (
         <div className="banner-container">
             <div className="banner-text-container">
+                <img src={logo} alt="logo" />
+                <img src={background} alt="background" />
                 <div className="banner-text">Chez vous, partout et ailleurs</div>
             </div>
         </div>
