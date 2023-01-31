c2fd1f5d389fe4c112bffd7b356ccbddfe3e4623
Ben <meteoben@gmail.com>
2023-01-31 18:07:52 +0100

    page about le collapse est pareil que sur fichelogement pas encore d'animation

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.svg";
-// import background from "../../images/background-banner.png";
+
 import "./Banner.css";
 
 export default function Banner() {
     return (
         <header className="banner-container">
             <div className="banner-text-container">
-
                 <img src={logo} alt="logo" />
                 <nav className="menu">
                     <Link to="/">Accueil</Link>
                     <Link to="/a-propos">A propos</Link>
                 </nav>
-                {/* <img src={background} alt="background" className="fond-home" /> */}
-                {/* <div className="banner-text">Chez vous, partout et ailleurs</div> */}
 
             </div>
         </header>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,27 +1,24 @@
 import React from "react";
-
-import { Link } from "react-router-dom";
 import Collapsible from "../../components/Collapsible/Collapsible";
 import aboutTab from "../../Tableau.json";
-import logo from "../../images/logo.svg";
 import backgroundAbout from "../../images/about-background.png";
 
 export default function About() {
     return (
 
         <div>
-            <img src={logo} alt="logo" />
-            <Link to="/">Accueil</Link>
-            <img src={backgroundAbout} alt="background" />
-            {aboutTab.map((rule, id) => (
-                <Collapsible
-                    key={id}
-                    aboutTitle={rule.Title}
-                    aboutText={rule.Text}
-                    aboutStyle="about-style"
-                />
+            <main>
+                <img src={backgroundAbout} alt="background" />
+                {aboutTab.map((rule, id) => (
+                    <Collapsible
+                        key={id}
+                        aboutTitle={rule.Title}
+                        aboutText={rule.Text}
+                        aboutStyle="about-style"
+                    />
 
-            ))}
+                ))}
+            </main>
         </div>
     );
 }
