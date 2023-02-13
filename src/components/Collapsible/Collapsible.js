16e4c56d0cdc24b342b270c2efa6b914861f63be
Ben <meteoben@gmail.com>
2023-02-13 19:44:50 +0100

    dernier commit après lionel sur fonctions collapsible

--- src/components/Collapsible/Collapsible.js
+++ src/components/Collapsible/Collapsible.js
@@ -1,31 +1,47 @@
 // import { getLogements, getOneLogement } from "../services/dataManager";
-import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+import { motion, useReducedMotion } from "framer-motion";
 import "./collapsible.css";
 // import vectorBase from "../images/assets_images/vectorBas.svg";
 // import vectorHaut from "../images/assets_images/vectorHaut.svg";
 
 const Collapsible = (props) => {
-
     console.log(props);
     const [toggle, setToggle] = useState(false);
+    const [height, setHeight] = useState("0rem");
+    const isList = Array.isArray(props.aboutText);
+    const style = {
+        characterHeight: 26,
+        characterWidth: 10,
+        padding: 54
+    };
 
     const toggleState = () => {
         setToggle(!toggle);
     };
 
+    const ref = useRef(null);
+    useEffect(() => {
+        const width = ref.current ? ref.current.offsetWidth : 1;
+        setHeight(roundSize(isList
+            ? estimateRows(props.aboutText, style.characterWidth, width)
+            : rowsFollowingText(props.aboutText, style.characterWidth, width)
+            , style
+        ));
+    }, [ref.current]);
+
     return (
-        <div className="collapsible" onClick={toggleState}>
+        <div className="collapsible" onClick={toggleState} ref={ref}>
             <div className={`chevron ${toggle ? "unfold" : ""}`}>{props.aboutTitle}</div>
 
             {toggle && (
                 <motion.div
                     className="toggle"
                     initial={{ height: 0 }}
-                    animate={{ height: "10rem" }}
+                    animate={{ height: height }}
                     transition={{ duration: 0.5 }}
                 >
-                    {Array.isArray(props.aboutText)
+                    {isList
                         ? <MakeList list={props.aboutText} />
                         : `${props.aboutText}`
                     }</motion.div>
@@ -47,6 +63,22 @@ function MakeList({ list }) {
             }
         </ul>
     );
+}
+
+function rowsFollowingText(text, characterWidth, divWidth) {
+    console.log("rows", text, text.length, characterWidth, divWidth, Math.ceil((text.length * characterWidth) / divWidth));
+    return Math.ceil((text.length * characterWidth) / divWidth);
+}
+
+function estimateRows(list, characterSize, width) {
+    let rows = 0;
+    for (const entry of list) {
+        rows += rowsFollowingText(entry, characterSize, width);
+    }
+    return rows;
+}
+function roundSize(rows, style) {
+    return Math.ceil((style.characterHeight * rows) + style.padding) + "px";
 }
 
 // import React, { useState } from "react";
