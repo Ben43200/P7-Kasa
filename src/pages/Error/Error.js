3e76fc5ae501819fa3c8bc3d26c780827cbf90fc
Ben <meteoben@gmail.com>
2023-03-06 17:19:21 +0100

    mise en place eroorpage pas encore ok pb grid

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -6,7 +6,7 @@ export default function Error() {
     return (
         <main>
             <div className="errorContainer">
-                <p className="errorNumber">404</p>
+                <p className="errorTitle">404</p>
                 <p className="errorText">Oups! La page que vous demandez n'existe pas.</p>
                 <Link to="/" className="errorLinkHome">
                     Retourner sur la page d’accueil
