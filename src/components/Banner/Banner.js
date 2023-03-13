a139a99c67056402323bb0c601b8213960e18725
Ben <meteoben@gmail.com>
2023-03-13 19:31:30 +0100

    ajout navlink suppression du package framer motion

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../../images/logo.svg";
 
 import "./Banner.scss";
@@ -10,14 +10,9 @@ export default function Banner() {
             <div className="banner-text-container">
                 <img src={logo} alt="logo" className="logo" />
                 <nav className="menu">
-                    <Link to="/">Accueil</Link>
-                    <Link to="/a-propos">A propos</Link>
+                    <NavLink to="/" >Accueil</NavLink>
+                    <NavLink to="/a-propos">A propos</NavLink>
                 </nav>
-                {/* // <Nav> */}
-                {/* <NavLink exact activeClassName='is-active' to='/'>Home</NavLink>
-                    <NavLink activeClassName='is-active' to='/a-propos'>About</NavLink> */}
-                {/* // </Nav> */}
-                {/* </nav> */}
             </div>
         </header>
     );
